d46732933b033be68f4d4b3cb06f4bcd0d1f0e65
Scott McFarlane <scott@mcfarlane.dev>
2020-04-12 17:59:35 +0100

    CodeMirror added to project

--- app/components/CodeEditor.tsx
+++ app/components/CodeEditor.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
-
+import { UnControlled as CodeMirror } from 'react-codemirror2';
 import { Editor } from '../stores/editor';
+import './codeeditor.css';
 
 type Props = {
   data: Editor;
 };
 
 function CodeEditor({ data }: Props) {
-  return <div />;
+  console.log(data);
+  return (
+    <CodeMirror
+      options={{
+        mode: 'javascript',
+        theme: 'material',
+        lineNumbers: true
+      }}
+    />
+  );
 }
 
 export default CodeEditor;
